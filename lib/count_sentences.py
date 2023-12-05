d2bac4bae237fbee797cd59bee0217a7b7855f9e
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-12-05 14:10:25 +1100

    complete

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,26 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            return("The value must be a string.")
+        else: self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        if not self.value:
+            return 0
+        # Split the string using regular expression to handle various punctuation marks
+        sentences = re.split(r'[.!?]+', self.value)
+        # Filter out empty strings from the list
+        non_empty_sentences = [s.strip() for s in sentences if s.strip()]
+        return len(non_empty_sentences)
+
